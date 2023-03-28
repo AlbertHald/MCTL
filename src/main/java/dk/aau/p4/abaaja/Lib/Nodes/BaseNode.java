40b6348e8e1564f60fe1d391938bad1eb1ed07c8
BaBrixx <bastian.b.hansen@gmail.com>
2023-03-28 15:25:18 +0200

    Implemented BaseNode.java

---
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/BaseNode.java
@@ -0,0 +1,52 @@
+package dk.aau.p4.abaaja.Lib.Nodes;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class BaseNode {
+    /**
+     * Get / Set methods for private variables
+     */
+    public List<BaseNode> get_children() {
+        return _children;
+    }
+    public void set_children(List<BaseNode> _children) {
+        this._children = _children;
+    }
+
+    public int get_lineNumber() {
+        return _lineNumber;
+    }
+    public void set_lineNumber(int _lineNumber) {
+        this._lineNumber = _lineNumber;
+    }
+
+    public String get_type() {
+        return _type;
+    }
+    public void set_type(String _type) {
+        this._type = _type;
+    }
+
+    /**
+     * Private variables
+     */
+    private int _lineNumber = 0;
+    private List<BaseNode> _children = new ArrayList<>();
+    private String _type;
+
+
+    /**
+     * Method for returning the unique hash code for the specific instance
+     * @return String representation of the unique hash code
+     */
+    public String getNodeHash() {
+        return String.valueOf(this.hashCode());
+    }
+
+    /**
+     * Accept method to make visitor visit the specific Node // TODO: Confirm this?
+     * @param visitor
+     */
+    public abstract void accept(INodeVisitor visitor);
+}
--- src/main/java/dk/aau/p4/abaaja/Main.java
+++ src/main/java/dk/aau/p4/abaaja/Main.java
@@ -1,9 +1,28 @@
 package dk.aau.p4.abaaja;
 
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        syntaxPhase( CharStreams.fromString("variable test: NUMBER;"));
+    }
 
+    private static ParseTree syntaxPhase(CharStream inputStream) {
+        //Scans the source code
+        mctlLexer lexer = new mctlLexer(inputStream);
+        lexer.removeErrorListeners();
+        CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+
+        //Parses the source code
+        mctlParser parser = new mctlParser(commonTokenStream);
+        parser.setBuildParseTree(true);
+        parser.removeErrorListeners();
+
+        return parser.mctl();
     }
 }
+
