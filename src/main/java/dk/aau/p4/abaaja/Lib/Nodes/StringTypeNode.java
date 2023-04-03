eeaf0156e8e2a38ae9062541c1998955f7b2867c
Anton <dev@atjn.dk>
2023-04-03 10:58:53 +0200

    Implement type nodes

--- src/main/java/dk/aau/p4/abaaja/Lib/Nodes/StringTypeNode.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/StringTypeNode.java
@@ -1,6 +1,11 @@
 package dk.aau.p4.abaaja.Lib.Nodes;
 
 public class StringTypeNode extends TypeNode {
+
+    public String get_type() {
+        return "STRING";
+    }
+
     @Override
     public void accept(INodeVisitor visitor) {
         visitor.visit(this);
