75ac96ee3bef891cf55a914db560bd70c979f618
AlbertHald <43075950+AlbertHald@users.noreply.github.com>
2023-04-03 11:09:27 +0200

    Created implementation for VarDecNode.java

--- src/main/java/dk/aau/p4/abaaja/Lib/Nodes/VarDecNode.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/VarDecNode.java
@@ -2,7 +2,22 @@ package dk.aau.p4.abaaja.Lib.Nodes;
 
 public class VarDecNode extends DecNode {
 
+    private String _id;
+    private TypeNode _type;
 
+    //Getters and setters
+    public String get_varDecId() {
+        return _id;
+    }
+    public void set_varDecId(String id){
+        this._id = id;
+    }
+    public TypeNode get_varDecType(){
+        return _type;
+    }
+    public void set_varDecType(TypeNode type){
+        this._type = type;
+    }
     @Override
     public void accept(INodeVisitor visitor) {
         visitor.visit(this);
