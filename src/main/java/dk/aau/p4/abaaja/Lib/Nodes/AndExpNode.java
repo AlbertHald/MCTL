fc1834012f87eaad2945dd5a42cee50284c15b19
Anton <dev@atjn.dk>
2023-04-04 08:57:29 +0200

    Implement expression nodes

--- src/main/java/dk/aau/p4/abaaja/Lib/Nodes/AndExpNode.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/AndExpNode.java
@@ -3,6 +3,17 @@ package dk.aau.p4.abaaja.Lib.Nodes;
 import dk.aau.p4.abaaja.Lib.Visitors.INodeVisitor;
 
 public class AndExpNode extends ExpNode {
+
+    public boolean get_result() {
+        return _result;
+    }
+
+    public void set_result(boolean result) {
+        this._result = result;
+    }
+
+    private boolean _result;
+    
     @Override
     public void accept(INodeVisitor visitor) {
         visitor.visit(this);
--- src/main/java/dk/aau/p4/abaaja/Lib/Nodes/StringExpNode.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/StringExpNode.java
@@ -3,6 +3,17 @@ package dk.aau.p4.abaaja.Lib.Nodes;
 import dk.aau.p4.abaaja.Lib.Visitors.INodeVisitor;
 
 public class StringExpNode extends ExpNode {
+
+    public String get_result() {
+        return _result;
+    }
+
+    public void set_result(String result) {
+        this._result = result;
+    }
+
+    private String _result;
+
     @Override
     public void accept(INodeVisitor visitor) {
         visitor.visit(this);
