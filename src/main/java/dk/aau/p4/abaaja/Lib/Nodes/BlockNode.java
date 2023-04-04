1592cc83acf2cbaa27aa7a3144edf32d5c50e747
AlbertHald <43075950+AlbertHald@users.noreply.github.com>
2023-04-04 09:17:19 +0200

    Created implementation for the last nodes

--- src/main/java/dk/aau/p4/abaaja/Lib/Nodes/BlockNode.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/BlockNode.java
@@ -2,7 +2,19 @@ package dk.aau.p4.abaaja.Lib.Nodes;
 
 import dk.aau.p4.abaaja.Lib.Visitors.INodeVisitor;
 
+import javax.sound.sampled.Line;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlockNode extends BaseNode {
+    private List<LineNode> _line = new ArrayList<>();
+
+    public List<LineNode> get_line() {
+        return _line;
+    }
+    public void set_line(List<LineNode> line) {
+        this._line = line;
+    }
     @Override
     public void accept(INodeVisitor visitor) {
         visitor.visit(this);
--- src/main/java/dk/aau/p4/abaaja/Lib/Nodes/RepeatStateNode.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/RepeatStateNode.java
@@ -3,6 +3,24 @@ package dk.aau.p4.abaaja.Lib.Nodes;
 import dk.aau.p4.abaaja.Lib.Visitors.INodeVisitor;
 
 public class RepeatStateNode extends StateNode {
+    //Private variables
+    private ExpNode _expression;
+    private BlockNode _block;
+
+    //Getters and setters
+    public ExpNode get_repeatExp(){
+        return _expression;
+    }
+    public void set_repeatExp(ExpNode expression) {
+        this._expression = expression;
+    }
+
+    public BlockNode get_expBlock() {
+        return _block;
+    }
+    public  void set_expBlock(BlockNode block) {
+        this._block = block;
+    }
     @Override
     public void accept(INodeVisitor visitor) {
         visitor.visit(this);
