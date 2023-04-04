7b62331726ab4d866c25394224104d8252bedeb6
AlbertHald <43075950+AlbertHald@users.noreply.github.com>
2023-04-04 09:17:19 +0200

    Created implementation for the last nodes

--- src/main/java/dk/aau/p4/abaaja/Lib/Nodes/AssStateNode.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/AssStateNode.java
@@ -3,6 +3,24 @@ package dk.aau.p4.abaaja.Lib.Nodes;
 import dk.aau.p4.abaaja.Lib.Visitors.INodeVisitor;
 
 public class AssStateNode extends StateNode {
+
+    //Private variables
+    private String _id;
+    private ExpNode _expression;
+
+    //getters and setters
+    public String get_assignId(){
+        return _id;
+    }
+    public void set_assignId(String id) {
+        this._id = id;
+    }
+    public ExpNode get_assignExp(){
+        return _expression;
+    }
+    public void set_assignExp(ExpNode expression) {
+        this._expression = expression;
+    }
     @Override
     public void accept(INodeVisitor visitor) {
         visitor.visit(this);
--- src/main/java/dk/aau/p4/abaaja/Lib/Nodes/StructDecNode.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/StructDecNode.java
@@ -2,8 +2,28 @@ package dk.aau.p4.abaaja.Lib.Nodes;
 
 import dk.aau.p4.abaaja.Lib.Visitors.INodeVisitor;
 
-public class StructDecNode extends DecNode {
+import java.util.ArrayList;
+import java.util.List;
 
+public class StructDecNode extends DecNode {
+    //Private
+    private String _id;
+    private List<VarDecNode> _declarations = new ArrayList<>();
+
+    public String get_structDecId() {
+        return _id;
+    }
+    public void set_structDecId(String id) {
+        this._id = id;
+    }
+
+    public List<VarDecNode> get_declarations() {
+        return _declarations;
+    }
+
+    public void set_declarations(List<VarDecNode> declarations){
+        this._declarations = declarations;
+    }
     @Override
     public void accept(INodeVisitor visitor) {
         visitor.visit(this);
