93a67929bd0a4ad850aa30567daa90a3e86ccd86
Jesper276 <90216888+Jesper276@users.noreply.github.com>
2023-04-06 15:13:47 +0200

    implementet invoke note
    Co-Authored-By: Nyby <45389841+NybyDK@users.noreply.github.com>

--- src/main/java/dk/aau/p4/abaaja/Lib/Nodes/InvokeNode.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/InvokeNode.java
@@ -3,6 +3,23 @@ package dk.aau.p4.abaaja.Lib.Nodes;
 import dk.aau.p4.abaaja.Lib.Visitors.INodeVisitor;
 
 public class InvokeNode extends StateNode {
+    //Private variables
+    private String _id;
+    private ExpNode _actualParameters;
+
+    //getters and setters
+    public String get_invokeId(){
+        return _id;
+    }
+    public void set_invokeId(String id) {
+        this._id = id;
+    }
+    public ExpNode get_paramExp(){
+        return _actualParameters;
+    }
+    public void set_paramExp(ExpNode expression) {
+        this._actualParameters = expression;
+    }
     @Override
     public void accept(INodeVisitor visitor) {
         visitor.visit(this);
