b687250da4cf302dc742ba343320d35779421363
Anton <dev@atjn.dk>
2023-04-12 13:32:29 +0200

    Add missing visitor types

--- src/main/java/dk/aau/p4/abaaja/Lib/Visitors/INodeVisitor.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Visitors/INodeVisitor.java
@@ -36,7 +36,10 @@ public interface INodeVisitor {
     void visit(TypecastExpNode node);
     void visit(UnaryExpNode node);
 
+    void visit(IDArrayExpNode node);
     void visit(IDExpNode node);
+    void visit(ActualIDExpNode node);
+    void visit(IDStructNode node);
     void visit(IDTypeNode node);
     void visit(BoolExpNode node);
     void visit(BoolTypeNode node);
