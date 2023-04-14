19432b5c69014b37efd880c7d3db2898f511bf10
BaBrixx <bastian.b.hansen@gmail.com>
2023-04-14 10:42:36 +0200

    Unit test setup completed

---
+++ src/test/java/dk/aau/p4/abaaja/VisitorTests/AstBuilderUnitTests.java
@@ -0,0 +1,18 @@
+package dk.aau.p4.abaaja.VisitorTests;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@Test()
+public class AstBuilderUnitTests {
+    @DataProvider
+    public Object[][] data() {
+        return new String[][] {new String[] {"data1"}, new String[] {"data2"}};
+    }
+
+    @Test(dataProvider = "data")
+    public void test(String d) {
+        Assert.assertEquals("First Line\nSecond Line", "First Line\nSecond Line");
+    }
+}
