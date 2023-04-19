e884ceb2d720b7da46e0632013bd75019325cbb5
AlbertHald <43075950+AlbertHald@users.noreply.github.com>
2023-04-19 12:54:54 +0200

    Initial SymbolTableUnitTests

---
+++ src/test/java/dk/aau/p4/abaaja/SymbolTableTests/SymbolTableUnitTests.java
@@ -0,0 +1,15 @@
+package dk.aau.p4.abaaja.SymbolTableTests;
+
+import dk.aau.p4.abaaja.Lib.Symbols.SymbolTable;
+import org.testng.annotations.Test;
+
+@Test()
+public class SymbolTableUnitTests {
+    SymbolTable testSymbolTable = new SymbolTable();
+
+    @Test
+    public void SymbolTableConstructor_ReturnsSymbolTableWithGlobalScope() {
+
+    }
+
+}
