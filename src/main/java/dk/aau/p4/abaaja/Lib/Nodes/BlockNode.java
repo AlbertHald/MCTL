7382aaf31f4488c7ed8fa6d3d76e7d3520f0bb35
Anton <dev@atjn.dk>
2023-04-19 14:43:19 +0200

    Simplify BlockNode

--- src/main/java/dk/aau/p4/abaaja/Lib/Nodes/BlockNode.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/BlockNode.java
@@ -6,16 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BlockNode extends BaseNode {
-    private List<LineNode> _line = new ArrayList<>();
-
-    public List<LineNode> get_line() {
-        return _line;
-    }
-    public void set_line(List<LineNode> line) {
-        this._line = line;
-    }
-    public void add_line(LineNode line) { this._line.add(line); }
-
     @Override
     public void accept(INodeVisitor visitor) {
         visitor.visit(this);
