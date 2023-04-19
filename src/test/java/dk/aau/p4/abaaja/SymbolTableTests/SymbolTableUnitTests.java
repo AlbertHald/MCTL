2849b665a0922248249ce9d2890bf7e77ea27701
AlbertHald <43075950+AlbertHald@users.noreply.github.com>
2023-04-19 13:37:00 +0200

    Made get_current unit test.

--- src/test/java/dk/aau/p4/abaaja/SymbolTableTests/SymbolTableUnitTests.java
+++ src/test/java/dk/aau/p4/abaaja/SymbolTableTests/SymbolTableUnitTests.java
@@ -1,15 +1,31 @@
 package dk.aau.p4.abaaja.SymbolTableTests;
 
+import dk.aau.p4.abaaja.Lib.Symbols.Symbol;
+import dk.aau.p4.abaaja.Lib.Symbols.Scope;
 import dk.aau.p4.abaaja.Lib.Symbols.SymbolTable;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @Test()
 public class SymbolTableUnitTests {
-    SymbolTable testSymbolTable = new SymbolTable();
+    SymbolTable testSymbolTable;
 
+    @BeforeMethod
+    public void BeforeMethod() {testSymbolTable = new SymbolTable();}
+    @AfterMethod()
+    public void AfterMethod() {testSymbolTable = null;}
     @Test
-    public void SymbolTableConstructor_ReturnsSymbolTableWithGlobalScope() {
+    public void GetCurrentScope_ScopeClass_ReturnsTheGlobalScope() {
 
+        //Arrange
+        testSymbolTable = new SymbolTable();
+        Scope testScope = new Scope("Global");
+        //Act
+        boolean result = testSymbolTable.get_currentScope() == testScope;
+
+        //Assert
+        assert(result);
     }
 
 }
