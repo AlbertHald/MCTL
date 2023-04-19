a54b01cb55db73227ffdbcc621fa992a3318e173
Anton <dev@atjn.dk>
2023-04-19 14:46:14 +0200

    Separate base ID and array accessors on IDArrayNode

--- src/main/java/dk/aau/p4/abaaja/Lib/Nodes/IDArrayExpNode.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Nodes/IDArrayExpNode.java
@@ -1,4 +1,11 @@
 package dk.aau.p4.abaaja.Lib.Nodes;
 
 public class IDArrayExpNode extends IDExpNode {
+    private IDExpNode _IDNode;
+    public void set_IDNode(IDExpNode IDNode){
+        this._IDNode = IDNode;
+    }
+    public IDExpNode get_IDNode(){
+        return this._IDNode;
+    }
 }
