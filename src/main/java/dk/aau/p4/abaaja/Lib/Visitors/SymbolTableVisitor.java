11cdf73696e0de07dd62d17f10b0e64d987638c4
BaBrixx <bastian.b.hansen@gmail.com>
2023-04-27 13:17:30 +0200

    Created TypeCheckingVisitor class

--- src/main/java/dk/aau/p4/abaaja/Lib/Visitors/SymbolTableVisitor.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Visitors/SymbolTableVisitor.java
@@ -118,6 +118,7 @@ public class SymbolTableVisitor implements INodeVisitor {
 
             if (typeDescriptor != null) {
                 Symbol symbol = new Symbol(node.get_id(), typeDescriptor);
+                symbol.set_isInstantiated(true);
                 symbolTable.insertSymbol(symbol);
             } else {
                 problemCollection.addProblem(ProblemType.ERROR_UNKNOWN_TYPE, "The type \"" + node.get_varDecType().get_type() + "\" does not exist", node.get_lineNumber());
@@ -144,6 +145,7 @@ public class SymbolTableVisitor implements INodeVisitor {
             }
 
             // Adding parameter to functionSymbol and current symbol table
+            paramSymbol.set_isInstantiated(true);
             symbolTable.insertSymbol(paramSymbol);
         }
 
---
+++ src/main/java/dk/aau/p4/abaaja/Lib/Visitors/TypeCheckingVisitor.java
@@ -0,0 +1,173 @@
+package dk.aau.p4.abaaja.Lib.Visitors;
+
+import dk.aau.p4.abaaja.Lib.Nodes.*;
+import dk.aau.p4.abaaja.Lib.Symbols.TypeDescriptors.MctlTypeDescriptor;
+
+public class TypeCheckingVisitor {
+
+    public MctlTypeDescriptor visit(MctlNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(LineNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(BlockNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(DecNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(StateNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(VarDecNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(FuncDecNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(StructDecNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(IfStateNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(RepeatStateNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(AssStateNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(InvokeNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(FuncInvokeNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(VarMethodInvokeNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(StringMethodInvokeNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(ReturnNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(FormalParamNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(StopNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(TypeNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(BoolTypeNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(NumTypeNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(StringTypeNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(NothingTypeNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(IDTypeNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(ExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(UnaryExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(TypecastExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(BinaryExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(MulExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(AddExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(AndExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(OrExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(CompExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(EqualExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(IDExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(ActualIDExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(IDArrayExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(IDStructNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(BoolExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(NumExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(StringExpNode node) {
+
+    }
+
+    public MctlTypeDescriptor visit(CommentNode node) { return null; }
+}
