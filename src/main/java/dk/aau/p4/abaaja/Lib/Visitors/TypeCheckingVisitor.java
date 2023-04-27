7d7c05c09032b9664fae1d15adc5490bd3647fac
Nyby <nyby.dev@gmail.com>
2023-04-27 15:18:45 +0200

    Visitor for NumExpNode + return nulls

--- src/main/java/dk/aau/p4/abaaja/Lib/Visitors/TypeCheckingVisitor.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/Visitors/TypeCheckingVisitor.java
@@ -17,107 +17,107 @@ public class TypeCheckingVisitor {
     }
 
     public MctlTypeDescriptor visit(MctlNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(LineNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(BlockNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(DecNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(StateNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(VarDecNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(FuncDecNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(StructDecNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(IfStateNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(RepeatStateNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(AssStateNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(InvokeNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(FuncInvokeNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(VarMethodInvokeNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(StringMethodInvokeNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(ReturnNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(FormalParamNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(StopNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(TypeNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(BoolTypeNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(NumTypeNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(StringTypeNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(NothingTypeNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(IDTypeNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(ExpNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(UnaryExpNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(TypecastExpNode node) {
@@ -125,35 +125,35 @@ public class TypeCheckingVisitor {
     }
 
     public MctlTypeDescriptor visit(BinaryExpNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(MulExpNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(AddExpNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(AndExpNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(OrExpNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(CompExpNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(EqualExpNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(IDExpNode node) {
-
+        return null; // TODO: Implement
     }
 
     public MctlTypeDescriptor visit(ActualIDExpNode node) {
@@ -173,7 +173,7 @@ public class TypeCheckingVisitor {
     }
 
     public MctlTypeDescriptor visit(NumExpNode node) {
-
+        return _symbolTable.searchType("NUMBER");
     }
 
     public MctlTypeDescriptor visit(StringExpNode node) {
