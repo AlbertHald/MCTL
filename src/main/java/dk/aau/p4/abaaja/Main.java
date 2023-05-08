0daf920e8198b2a9313b818860cb529352048e30
BaBrixx <90306530+BaBrixx@users.noreply.github.com>
2023-05-08 09:02:08 +0200

    Update src/main/java/dk/aau/p4/abaaja/Main.java
    Undo main.java changes for easier merging
    
    Co-authored-by: Anton <dev@atjn.dk>

--- src/main/java/dk/aau/p4/abaaja/Main.java
+++ src/main/java/dk/aau/p4/abaaja/Main.java
@@ -23,7 +23,7 @@ public class Main {
         ProblemCollection problemCollection = new ProblemCollection();
 
         // Parse test CharStream
-        ParseTree tree = syntaxPhase( CharStreams.fromString("variable test: BOOLEAN; test = var1 == var2;"), problemCollection);
+        ParseTree tree = syntaxPhase( CharStreams.fromString("variable test:NUMBER; \nvariable bing: BOOLEAN; \ntest = 2 + bing;"), problemCollection);
 
         if (!problemCollection.getHasErrors()) {
             // Continue parsing here
