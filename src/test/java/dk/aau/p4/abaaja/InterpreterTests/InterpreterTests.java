55b700bff13f0c5fdaaa59785e7a53dc82d9df60
Nyby <nyby.dev@gmail.com>
2023-05-15 09:48:48 +0200

    Update InterpreterTests.java

--- src/test/java/dk/aau/p4/abaaja/InterpreterTests/InterpreterTests.java
+++ src/test/java/dk/aau/p4/abaaja/InterpreterTests/InterpreterTests.java
@@ -1172,6 +1172,7 @@ public class InterpreterTests {
                 {"variable test: BOOLEAN[]; variable l: BOOLEAN[]; l[0] = true; l[1] = false; l[2] = true; test = l.sublist(0, 1);", List.of(true, false)},
                 {"variable test: BOOLEAN[]; variable l: BOOLEAN[]; l[0] = true; l[1] = false; l[2] = true; test = l.sublist(1, 2);", List.of(false, true)},
                 {"variable test: BOOLEAN[]; variable l: BOOLEAN[]; l[0] = true; l[1] = false; l[2] = true; test = l.sublist(0, 2);", List.of(true, false, true)},
+                {"variable test: BOOLEAN[]; variable l: BOOLEAN[]; l[0] = true; l[1] = false; l[2] = true; test = l.sublist(1);", List.of(false, true)},
         };
     }
     @Test(dataProvider = "listSublistTestData_bool")
@@ -1210,6 +1211,7 @@ public class InterpreterTests {
                 {"variable test: NUMBER[]; variable l: NUMBER[]; l[0] = 1; l[1] = 2; l[2] = 3; test = l.sublist(0, 1);", List.of(1.0, 2.0)},
                 {"variable test: NUMBER[]; variable l: NUMBER[]; l[0] = 1; l[1] = 2; l[2] = 3; test = l.sublist(1, 2);", List.of(2.0, 3.0)},
                 {"variable test: NUMBER[]; variable l: NUMBER[]; l[0] = 1; l[1] = 2; l[2] = 3; test = l.sublist(0, 2);", List.of(1.0, 2.0, 3.0)},
+                {"variable test: NUMBER[]; variable l: NUMBER[]; l[0] = 1; l[1] = 2; l[2] = 3; test = l.sublist(1);", List.of(2.0, 3.0)},
         };
     }
     @Test(dataProvider = "listSublistTestData_number")
@@ -1248,6 +1250,7 @@ public class InterpreterTests {
                 {"variable test: STRING[]; variable l: STRING[]; l[0] = 'str1'; l[1] = 'str2'; l[2] = 'str3'; test = l.sublist(0, 1);", List.of("str1", "str2")},
                 {"variable test: STRING[]; variable l: STRING[]; l[0] = 'str1'; l[1] = 'str2'; l[2] = 'str3'; test = l.sublist(1, 2);", List.of("str2", "str3")},
                 {"variable test: STRING[]; variable l: STRING[]; l[0] = 'str1'; l[1] = 'str2'; l[2] = 'str3'; test = l.sublist(0, 2);", List.of("str1", "str2", "str3")},
+                {"variable test: STRING[]; variable l: STRING[]; l[0] = 'str1'; l[1] = 'str2'; l[2] = 'str3'; test = l.sublist(1);", List.of("str2", "str3")},
         };
     }
     @Test(dataProvider = "listSublistTestData_string")
