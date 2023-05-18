3118e57350908e2b6f4d406bd0e9c4c6508d45a9
BaBrixx <bastian.b.hansen@gmail.com>
2023-05-18 21:11:29 +0200

    Simplified problem handling

--- src/main/java/dk/aau/p4/abaaja/Lib/ProblemHandling/ProblemType.java
+++ src/main/java/dk/aau/p4/abaaja/Lib/ProblemHandling/ProblemType.java
@@ -4,9 +4,7 @@ package dk.aau.p4.abaaja.Lib.ProblemHandling;
  * Enumerator for errors and warnings.
  */
 public enum ProblemType {
-    /*
-    Add errors and warnings here:
-     */
+    // Add errors and warnings here:
     ERROR_PARSER("ERROR_PARSER"),
     ERROR_LEXER("ERROR_LEXER"),
     ERROR_AST_BUILDER("ERROR_AST_BUILDER"),
